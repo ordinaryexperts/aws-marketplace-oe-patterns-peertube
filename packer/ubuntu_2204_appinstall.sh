8191ff4b6665cf15b9db765c43b751b1716f9864
Dylan Vaughn <dylan@ordinaryexperts.com>
2023-08-20 22:31:27 -0700

    final 1.0.0 updates

--- packer/ubuntu_2204_appinstall.sh
+++ packer/ubuntu_2204_appinstall.sh
@@ -1,3 +1,4 @@
+
 SCRIPT_VERSION=1.2.5
 SCRIPT_PREINSTALL=ubuntu_2204_graviton_preinstall.sh
 SCRIPT_POSTINSTALL=ubuntu_2204_postinstall.sh
@@ -12,8 +13,128 @@ rm $SCRIPT_PREINSTALL
 # PeerTube configuration
 #
 
+# aws cloudwatch
+cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+{
+  "agent": {
+    "metrics_collection_interval": 60,
+    "run_as_user": "root",
+    "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
+  },
+  "metrics": {
+    "metrics_collected": {
+      "collectd": {
+        "metrics_aggregation_interval": 60
+      },
+      "disk": {
+        "measurement": ["used_percent"],
+        "metrics_collection_interval": 60,
+        "resources": ["*"]
+      },
+      "mem": {
+        "measurement": ["mem_used_percent"],
+        "metrics_collection_interval": 60
+      }
+    },
+    "append_dimensions": {
+      "ImageId": "\${aws:ImageId}",
+      "InstanceId": "\${aws:InstanceId}",
+      "InstanceType": "\${aws:InstanceType}",
+      "AutoScalingGroupName": "\${aws:AutoScalingGroupName}"
+    }
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/dpkg.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/dpkg.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/apt/history.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/apt/history.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/cloud-init.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/cloud-init.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/cloud-init-output.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/cloud-init-output.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/auth.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/auth.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/syslog",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/syslog",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/amazon/ssm/amazon-ssm-agent.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/amazon/ssm/amazon-ssm-agent.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/amazon/ssm/errors.log",
+            "log_group_name": "ASG_SYSTEM_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/amazon/ssm/errors.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/nginx/peertube.access.log",
+            "log_group_name": "ASG_APP_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/nginx/peertube.access.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/nginx/peertube.error.log",
+            "log_group_name": "ASG_APP_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/log/nginx/peertube.error.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/www/peertube/storage/logs/peertube.log",
+            "log_group_name": "ASG_APP_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/www/peertube/storage/logs/peertube.log",
+            "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/www/peertube/storage/logs/peertube-audit.log",
+            "log_group_name": "ASG_APP_LOG_GROUP_PLACEHOLDER",
+            "log_stream_name": "{instance_id}-/var/www/peertube/storage/logs/peertube-audit.log",
+            "timezone": "UTC"
+          }
+        ]
+      }
+    },
+    "log_stream_name": "{instance_id}"
+  }
+}
+EOF
+
 # https://github.com/Chocobozzz/PeerTube/releases
-VERSION=v5.1.0
+VERSION=v5.2.0
 
 apt-get update
 apt-get -y install curl sudo unzip vim
